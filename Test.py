ab51e4ffe4f60b2f03e98c9f6101baafa402b487
KuroNeko717 <joellc139@gmail.com>
2021-12-22 14:05:38 +0530

    added multi screen feature (credits:asher)

--- Test.py
+++ Test.py
@@ -8,17 +8,26 @@ class home_screen(QMainWindow):
     def __init__(self,ui_file):
         super(home_screen,self).__init__()
         uic.loadUi(ui_file,self)
+        self.create_schedule = None
         self.button_readmore_schedule_homescreen.clicked.connect(self.o_create_schedule)
         self.show()
 
     def o_create_schedule(self):
-        window=create_schedule("Ui_files\Read_More_Form.ui")
+        if self.create_schedule is None:
+            self.create_schedule = create_schedule("Ui_files\Read_More_Form.ui")
+            self.create_schedule.show()
+        else:
+            self.create_schedule.close()
+            self.create_schedule = None
+
 
 class create_schedule(QMainWindow):
     def __init__(self,ui_file):
         super(create_schedule,self).__init__()
         uic.loadUi(ui_file,self)
-        self.show()
+
+
+
 
 app=QApplication([])
 
