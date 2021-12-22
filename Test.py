4dd2a6eaf03524c48659cca08e55182a378e6f7b
BigJoe098 <joelashersamuel1234@gmail.com>
2021-12-22 12:03:51 +0530

    First Commit

---
+++ Test.py
@@ -0,0 +1,28 @@
+import sys
+import PyQt5 
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtGui import QWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+class home_screen(QMainWindow):
+    def __init__(self,ui_file):
+        super(home_screen,self).__init__()
+        uic.loadUi(ui_file,self)
+        self.button_readmore_schedule_homescreen.clicked.connect(self.o_create_schedule)
+        self.show()
+
+    def o_create_schedule(self):
+        window=create_schedule("Ui_files\Read_More_Form.ui")
+
+class create_schedule(QMainWindow):
+    def __init__(self,ui_file):
+        super(create_schedule,self).__init__()
+        uic.loadUi(ui_file,self)
+        self.show()
+
+app=QApplication([])
+
+if __name__ == "__main__":
+    window=home_screen("Ui_files\Home_Screen_Window.ui")
+    app.exec_()
+
